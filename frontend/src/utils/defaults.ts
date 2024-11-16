d903f52178a5ae3e74faba4ce92c2275d242989a
Ryan <ryanbprog@gmail.com>
2024-11-16 18:53:48 -0300

    added a file for app default values

---
+++ frontend/src/utils/defaults.ts
@@ -0,0 +1,11 @@
+export const DEFAULT_SORT = "date-dec";
+
+export const DEFAULT_TRANSACTION_DATA = {
+  transactions: [],
+  total: 0,
+  totalItems: 0,
+  totalPages: 0,
+  currentPage: 1,
+};
+
+export const PAGINATION_LIMIT = 10;
